8e147e93c746998ddc23b7652c121ad1cbfeb584
franedsandi <franedsandi@gmail.com>
2023-10-11 16:37:29 +0200

    [feat] card layout and desing scss

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,9 +5,33 @@ export default {
 </script>
 
 <template>
-<div class="container">
-
-</div>
+  <div class="mid">
+    <div class="container">
+      <div class="content">
+        <div class="card">
+          <div class="image p-relative">
+            <img src="../assets/img/1.webp" alt="articulo 1">
+            <img src="../assets/img/1b.webp" alt="articulo 1b" class="upper p-absolute">
+            <div class="like p-absolute">
+              <span class="heart" >&hearts;</span>
+            </div>
+            <div class="caracteristicas p-absolute">
+              <span class="sconto">50%</span>
+              <span class="sostenibile">Sostenibilità</span>
+            </div>
+          </div>
+            <div class="description">
+              <span class="tag brand">levi's</span>
+              <span class="tag name">relazed fit tee unisex</span>
+              <span class="tag price">
+              <span class="red">14.99 &euro;</span>
+              <de>29.99&euro;</de>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
